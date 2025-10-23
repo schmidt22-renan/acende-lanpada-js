be0c5632cd475702c853a850d68bfeaba4e657fc
schmidt22-renan <schmidt.renan@escola.pr.gov.br>
2025-10-23 12:17:19 +0000

    ff

---
+++ script.js
@@ -0,0 +1,9 @@
+function acender(){
+   // document.getElementById("lanpada").src="on.png"
+    console.log("On")
+}
+
+function apagar(){
+   // document.getElementById("lanpada").src="off.png"
+    console.log("off")
+}
